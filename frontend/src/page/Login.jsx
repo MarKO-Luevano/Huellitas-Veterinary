7f898009cf9fba79e848fb7346459d23f56faadd
Iansmai <L23051099@saltillo.tecnm.mx>
2026-06-04 17:42:51 -0600

    arreglar passw

--- frontend/src/page/Login.jsx
+++ frontend/src/page/Login.jsx
@@ -24,7 +24,7 @@ const navigate = useNavigate()
       const usuario = response.data
 
       if (usuario.password !== password) {
-        setError('Esa q')
+        setError('Contraseña incorrecta')
         setLoading(false)
         return
       }
